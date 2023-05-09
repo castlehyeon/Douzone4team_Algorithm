808b1885fdcf8399ecee1cb0972cd0d0501608be
castlehyeon <qnftktls118@naver.com>
2023-05-09 17:06:47 +0900

    0509html-video-audio first-commit

---
+++ Douzone4team-seonghyeon/project_4team/dragdrop.js
@@ -0,0 +1,17 @@
+$(function() {
+    // 플레이리스트 아이템 드래그 가능하게 설정
+    $("#playlist li").draggable({
+      helper: "clone",
+      cursor: "move",
+      zIndex: 9999,
+      revert: "invalid"
+    });
+  
+    // 비디오 플레이어 드랍 영역 드랍 가능하게 설정
+    $("#player").droppable({
+      drop: function(event, ui) {
+        const videoUrl = $(ui.draggable).data("video");
+        $("#player").html(`<video src="${videoUrl}" controls autoplay></video>`);
+      }
+    });
+  });
